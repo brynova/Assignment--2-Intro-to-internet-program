6dc7bb8ac3110edb3af9047b1fb23794a00bc3e1
brynova <bryant321e@gmail.com>
2024-04-01 11:21:51 -0400

    Added the temple for the validator to modify

--- assignment-2--intro-to-internet-programming--cs-275--spring-2024-master/js/main.js
+++ assignment-2--intro-to-internet-programming--cs-275--spring-2024-master/js/main.js
@@ -1,3 +1,23 @@
+
+
 window.onload = () => {
     alert(`Test`);
 };
+// Place the styles
+// 1.) Weights of 300 & 600
+// 2.) Pixel size = 16 px.
+// 3.) Content Leading = 1.4
+// 4.) font size of h1 = 32 px.
+// 5.) leading of h1 element = 38 px.
+// 6.) width of each slide = 640 px
+// --------------> Left/Right padding = 20 px.
+
+//Editing the Weights:
+window.onload =() =>{
+
+};
+
+// Button Listeners
+ReportBody.addEventListener('Click',()=>{
+    alert('You just clicked!);
+});
